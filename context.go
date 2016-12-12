23012b580771bbbabdeb91de12d62793610e5010
Mingoo Kim <bearchit@gmail.com>
2016-12-12 18:09:14 +0900

    context helper function 정리
    - params helper 제거
    - BindJSON() 추가

--- context.go
+++ context.go
@@ -1,8 +1,40 @@
 package kiss
 
-import "net/http"
+import (
+	"net/http"
+
+	"github.com/gorilla/schema"
+	"github.com/unrolled/render"
+	"gopkg.in/square/go-jose.v1/json"
+)
 
 type Context struct {
 	ResponseWriter http.ResponseWriter
 	Request        *http.Request
+}
+
+func (c *Context) BindForm(decoder *schema.Decoder, v interface{}) error {
+	if err := c.Request.ParseForm(); err != nil {
+		return err
+	}
+
+	if err := decoder.Decode(v, c.Request.Form); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (c *Context) BindJSON(v interface{}) error {
+	decoder := json.NewDecoder(c.Request.Body)
+	if err := decoder.Decode(v); err != nil {
+		return err
+	}
+	defer c.Request.Body.Close()
+
+	return nil
+}
+
+func (c *Context) JSON(status int, v interface{}) {
+	render.New().JSON(c.ResponseWriter, status, v)
 }
