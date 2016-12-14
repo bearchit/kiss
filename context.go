c1bd252f473fa9d948178ec82fb76d63affa58e0
Mingoo Kim <bearchit@gmail.com>
2016-12-14 13:47:42 +0900

    + URL parameter parser

--- context.go
+++ context.go
@@ -3,6 +3,9 @@ package kiss
 import (
 	"net/http"
 
+	"strconv"
+
+	"github.com/gorilla/mux"
 	"github.com/gorilla/schema"
 	"github.com/unrolled/render"
 	"gopkg.in/square/go-jose.v1/json"
@@ -11,6 +14,23 @@ import (
 type Context struct {
 	ResponseWriter http.ResponseWriter
 	Request        *http.Request
+}
+
+func (c *Context) URLParams() map[string]string {
+	return mux.Vars(c.Request)
+}
+
+func (c *Context) URLParam(key string) string {
+	return c.URLParams()[key]
+}
+
+func (c *Context) URLParamUint(key string) (uint, error) {
+	v := c.URLParam(key)
+	iv, err := strconv.Atoi(v)
+	if err != nil {
+		return 0, err
+	}
+	return uint(iv), nil
 }
 
 func (c *Context) BindForm(decoder *schema.Decoder, v interface{}) error {
